7c0175856a59cdf47614b040a038aea2a67e5320
CitroenGames <54474809+CitroenGames@users.noreply.github.com>
2023-06-03 14:39:23 +0200

    optiomization belike

--- content.js
+++ content.js
@@ -1,13 +1,19 @@
-setInterval(() => {
-  const adElement1 = document.querySelector('.ytp-ad-player-overlay-flyout-cta.ytp-ad-player-overlay-flyout-cta-rounded');
-  const adElement2 = document.querySelector('.ytp-ad-text');
-  
-  if (adElement1 || adElement2) {
-    const videos = document.querySelectorAll('video');
-    for(let i=0; i<videos.length; i++) {
-      if (videos[i].currentTime < videos[i].duration) {
-        videos[i].currentTime = videos[i].duration;
+const observer = new MutationObserver((mutationsList, observer) => {
+  for(let mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      const adElement1 = document.querySelector('.ytp-ad-player-overlay-flyout-cta.ytp-ad-player-overlay-flyout-cta-rounded');
+      const adElement2 = document.querySelector('.ytp-ad-text');
+
+      if (adElement1 || adElement2) {
+        const videos = document.querySelectorAll('video');
+        for(let i=0; i<videos.length; i++) {
+          if (videos[i].currentTime < videos[i].duration) {
+            videos[i].currentTime = videos[i].duration;
+          }
+        }
       }
     }
   }
-}, 1000);
+});
+
+observer.observe(document, { attributes: false, childList: true, subtree: true });
