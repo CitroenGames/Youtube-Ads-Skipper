b0c5f17f89d62a2ac57aa5b8c606804d0c636a16
CitroenGames <54474809+CitroenGames@users.noreply.github.com>
2023-06-03 14:37:17 +0200

    fixed

--- content.js
+++ content.js
@@ -1,6 +1,8 @@
 setInterval(() => {
-  const adElement = document.querySelector('.ytp-ad-player-overlay-flyout-cta.ytp-ad-player-overlay-flyout-cta-rounded');
-  if (adElement) {
+  const adElement1 = document.querySelector('.ytp-ad-player-overlay-flyout-cta.ytp-ad-player-overlay-flyout-cta-rounded');
+  const adElement2 = document.querySelector('.ytp-ad-text');
+  
+  if (adElement1 || adElement2) {
     const videos = document.querySelectorAll('video');
     for(let i=0; i<videos.length; i++) {
       if (videos[i].currentTime < videos[i].duration) {
