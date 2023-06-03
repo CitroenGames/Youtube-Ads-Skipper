4405618e23a6f7613db8e11970e135889f3fbd68
CitroenGames <54474809+CitroenGames@users.noreply.github.com>
2023-06-03 15:17:22 +0200

    optimization by @avi12
    thenk u ig

--- content.js
+++ content.js
@@ -1,24 +1,39 @@
-const observer = new MutationObserver((mutationsList, observer) => {
-  for(let mutation of mutationsList) {
-    if (mutation.type === 'childList') {
-      const adElement1 = document.querySelector('.ytp-ad-player-overlay-flyout-cta.ytp-ad-player-overlay-flyout-cta-rounded');
-      const adElement2 = document.querySelector('.ytp-ad-text');
-
-      if (adElement1 || adElement2) {
-        const videos = document.querySelectorAll('video');
-        for(let i=0; i<videos.length; i++) {
-          if (videos[i].currentTime < videos[i].duration) {
-            videos[i].currentTime = videos[i].duration;
-          }
-        }
-      }
-
-      const skipAdButton = document.querySelector('.ytp-ad-skip-button.ytp-button');
-      if (skipAdButton) {
-        skipAdButton.click();
-      }
+function getVisibleElement(selector) {
+        const elements = document.querySelectorAll(selector);
+        return [...elements].find(element => element.offsetWidth && element.offsetHeight);
     }
-  }
-});
 
-observer.observe(document, { attributes: false, childList: true, subtree: true });
+    const options = { childList: true, subtree: true };
+
+    let VIDEO;
+
+    function getAd() {
+        return getVisibleElement(".ytp-ad-player-overlay-flyout-cta.ytp-ad-player-overlay-flyout-cta-rounded");
+    }
+
+    function skipAd() {
+        if (!getAd()) {
+            return;
+        }
+
+        VIDEO.currentTime = VIDEO.duration;
+    }
+
+    function prepareToSkipAd() {
+        new MutationObserver((_, observer) => {
+            const elVideo = getVisibleElement("video");
+            if (!elVideo) {
+                return;
+            }
+
+            observer.disconnect();
+
+            VIDEO = elVideo;
+            VIDEO.removeEventListener("canplay", skipAd);
+            VIDEO.addEventListener("canplay", skipAd);
+        }).observe(document, options);
+    }
+
+    new MutationObserver(prepareToSkipAd).observe(document.querySelector("title"), options);
+
+    prepareToSkipAd();
